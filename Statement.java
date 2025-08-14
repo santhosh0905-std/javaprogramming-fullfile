b91f509000727226461cfa01bd7355f05998c009
Santhosh.U <usanthosh0905@gmail.com>
2025-08-14 16:10:37 +0530

    Create Statement.java

---
+++ Statement.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int x=19;
+        int y=20;
+        if (x<y){
+        System.out.println("X is younger & Y is Elder");}
+        else {
+        System.out.println("X is Elderr & Y is younger");}
+    }
+}
